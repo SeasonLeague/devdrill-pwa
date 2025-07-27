2cfb348a494e012c5c8c49a389fa9d96df8adf2a
SeasonLeague <facebookups23@gmail.com>
2025-07-27 09:18:05 +0100

    [+] added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata, Viewport } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -67,6 +68,7 @@ export default function RootLayout({
       </head>
       <body className={`${inter.className} bg-slate-900 text-white min-h-screen`}>
         {children}
+        <Analytics />
         <Toaster />
       </body>
     </html>
